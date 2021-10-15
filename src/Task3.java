1877a63492e94fc6119c3bb974a5fb5313b549dc
nastre3 <nastre70@gmail.com>
2021-10-15 19:45:41 +1000

    Added Task3

---
+++ src/Task3.java
@@ -0,0 +1,20 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int x = 3;
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+            }
+            x = x - 1;
+            System.out.println("-");
+            if (x == 2) {
+                System.out.println("b c");
+            }
+            if (x == 1) {
+                System.out.println("d");
+                x = x - 1;
+            }
+
+        }
+    }
+}
