831d836dcec362afb747a635bfebea60c428fe85
nastre3 <nastre70@gmail.com>
2021-10-22 17:42:26 +1000

    Finished Task6

--- src/ConditionsAndCycles.java
+++ src/ConditionsAndCycles.java
@@ -2,28 +2,28 @@
 public class ConditionsAndCycles {
     public static void main(String[] args) {
         int a = 4, b = 3, c = 2;
-        b = b + c;
+        b = b + c; // 5
         System.out.println(b);
-        a = a * b;
+        //a = a * b; // 20
         System.out.println(a);
-        c = a - c;
+        c = a - c; // 18
         System.out.println(c);
-        a = a + b;
+        //a = a + b; // 25
         System.out.println(a);
-        a = a - b;
+        a = a - b; // 20
         System.out.println(a);
-        c = a + b;
+        c = a + b; // 25
         System.out.println(c);
-        b = c / a;
+        //b = c / a; // 1
         System.out.println(b);
-        a = b + c;
+        a = b + c; // 26
         System.out.println(a);
-        a = a + c + b;
+        a = a + c + b; // 52
         System.out.println(a);
 
         // Необходимо закомментировать некоторые из строк так, чтобы переменная a приняла значение 18. Вывести её в консоль 20 раз.
-       // for (int i = 0; i <20; i++) {
-       //     System.out.println(a);
-        //}
+        for (int i = 0; i < 20; i++) {
+            System.out.println(a);
+        }
     }
 }
