e812be205460f7b0f7b3a6eab19fa3dfaa04b037
nastre3 <nastre70@gmail.com>
2021-10-25 20:22:11 +1000

    Amended Max (Task A3) - added 2nd version

--- src/Max.java
+++ src/Max.java
@@ -2,8 +2,10 @@ public class Max {
     public static void main(String[] args) {
         max(5, 6, 7, 7);
     }
-public static void max(int a, int b, int c, int d) {
-        // write your code here
+
+    public static void max(int a, int b, int c, int d) {
+        // 1st version
+    /*
         if (a >= b) {
             if (a >= c) {
                 if (a >= d) {
@@ -19,5 +21,11 @@ public static void max(int a, int b, int c, int d) {
         } else if (d >= c) {
             System.out.println(d);
         }
+    */
+        //2nd version
+        int max1 = a > b ? a : b;
+        int max2 = c > d ? c : d;
+        int max = max1 > max2 ? max1 : max2;
+        System.out.println(max);
     }
 }
